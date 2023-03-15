20d25adc2f1e2c9099a696ec0f854edf302ad7a4
Patryk Czekaj <patryk.czekaj1215@gmail.com>
2023-03-15 17:50:28 +0100

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Form from './Form';
+import Result from './Result';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Form />
+      <Result />
+    </div>
+  );
+};
+
+export default App;
