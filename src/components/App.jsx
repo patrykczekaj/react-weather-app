4eb95038a51ca9469aad3fc919fca319e3025ed4
Patryk Czekaj <patryk.czekaj1215@gmail.com>
2023-03-15 18:36:48 +0100

    add api request

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,40 @@ import './App.css';
 const App = () => {
 
   const [value, setValue] = useState('');
+  const [date, setDate] = useState('');
+  const [city, setCity] = useState('');
+  const [sunrise, setSunrise] = useState('');
+  const [sunset, setSunset] = useState('');
+  const [temp, setTemp] = useState('');
+  const [pressure, setPressure] = useState('');
+  const [wind, setWind] = useState('');
+  const [err, setErr] = useState('');
+
+  const API = `https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=ea5196d684737306b38c3fa85e8c9aad`
 
   const handleInputChange = (e) => {
     setValue(e.target.value)
   }
 
+  const handleCitySubmit = (e) => {
+    e.preventDefault();
+
+    fetch(API)
+    .then(response => {
+      if (response.ok) {
+        return response
+      }
+      throw Error('an error occured')
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+
+  }
+
   return (
     <div className='App'>
-      <Form value={value} change={handleInputChange}/>
+      <Form value={value} change={handleInputChange} submit={handleCitySubmit}/>
       <Result />
     </div>
   );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Form = ({ value, change }) => {
+const Form = ({ value, change, submit }) => {
 
     return (
-        <form>
+        <form onSubmit={submit}>
             <input type="text" value={value} onChange={change} placeholder="type city..." />
             <button>Search City</button>
         </form>
